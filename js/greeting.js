2f227541d29e3f8932f478a6e448db53acc5d8d3
jiho_lee <wlgh0324@gmail.com>
2024-08-06 13:25:38 +0900

    logout 기능 구현
    logout 기능 추가

--- js/greeting.js
+++ js/greeting.js
@@ -1,31 +1,48 @@
-const greetingForm = document.querySelector("#greeting"); // html의 form을 가져옴
-const greeting = document.querySelector("#greeting input:first-child"); // 위 form의 input 태그를 가져옴
+const logInForm = document.querySelector("#logIn"); // logIn form을 가져옴
+const logInInput = document.querySelector("#logIn input:first-child"); // 위 form의 input 태그를 가져옴
+const logOutForm = document.querySelector("#logOut"); // logOut form을 가져옴
+
+const username = document.querySelector("h1#username"); // 로그인 된 유저를 표시할 h1태그
+const USER_NAME = "username"
+const HIDDEN = "hidden"
 
 // logIn 기능
 function logIn(event) {
     event.preventDefault();
-    const username = greeting.value;
+    const username = logInInput.value;
     showUser(username);
     saveUserName(username);
 }
 
+// logOut 기능
+function logOut(event) {
+    event.preventDefault();
+    localStorage.removeItem(USER_NAME);
+    username.classList.add(HIDDEN);
+    logInForm.classList.remove(HIDDEN);
+    logOutForm.classList.add(HIDDEN);
+}
+
 // logIn이 되었음을 표시하는 기능
 function showUser(user) {
-        const username = document.querySelector("h1#username");
-        username.classList.remove("hidden");
+        username.classList.remove(HIDDEN);
         username.innerText = user;
-        greetingForm.classList.add("hidden");
+        logInForm.classList.add(HIDDEN);
+        logOutForm.classList.remove(HIDDEN);
 }
 
 // 입력 사항을 localStorage에 저장
 function saveUserName(name) {
-    localStorage.setItem("username", name);
+    localStorage.setItem(USER_NAME, name);
 }
 
-// 제출이 발생했을 경우
-const checkUser = localStorage.getItem("username")
+// 로그인이 발생했을 경우
+const checkUser = localStorage.getItem(USER_NAME);
 if (checkUser) {
     showUser(checkUser)
 } else {
-    greetingForm.addEventListener("submit", logIn);
+    logInForm.addEventListener("submit", logIn);
 }
+
+// 로그아웃이 발생했을 경우
+logOutForm.addEventListener("submit", logOut);
